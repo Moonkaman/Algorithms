d9c031c4c151de9680a8bd30565f015a34e5db8a
Moonkaman <nickbatmanglidj@gmail.com>
2019-05-02 16:41:49 -0600

    Complete rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,30 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    elements = ['rock', 'paper', 'scissors']
+    possible_plays = []
+
+    if n == 0:
+        return [[]]
+    elif n == 1:
+        return elements
+    else:
+        rps = rock_paper_scissors(n-1)
+        for i in range(len(elements)):
+            for j in range(3**(n-1)):
+                possible_plays += [elements[i] + ' ' + rps[j]]
+
+    return possible_plays
 
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+print(rock_paper_scissors(3))
+
+
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_plays = int(sys.argv[1])
+#         print(rock_paper_scissors(num_plays))
+#     else:
+#         print('Usage: rps.py [num_plays]')
