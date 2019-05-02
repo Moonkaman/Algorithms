cbf785c9751a3649485ec5b86a089f58f3999d92
Moonkaman <nickbatmanglidj@gmail.com>
2019-05-02 16:42:13 -0600

    Complete making_change.py

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,42 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
+
+def making_change(amount, denominations, cache=None):
+    if cache == None:
+        cache = [0 for i in range(amount+1)]
+
+    cache[0] = 1
+
+    for i in denominations:
+        for higher_amount in range(i, amount+1):
+            diff = higher_amount - i
+            cache[higher_amount] += cache[diff]
+
+    return cache[amount]
 
 
-if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+'''
+0: 1
+1: 1
+2: 1
+3: 1
+4: 1
+5: 2
+6: 2
+7: 2
+8: 2
+9: 2
+10: 4
+'''
+
+
+# if __name__ == "__main__":
+#   # Test our your implementation from the command line
+#   # with `python making_change.py [amount]` with different amounts
+#   if len(sys.argv) > 1:
+denominations = [1, 5, 10, 25, 50]
+print(making_change(12, denominations))
+#   print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+# else:
+#   print("Usage: making_change.py [amount]")
